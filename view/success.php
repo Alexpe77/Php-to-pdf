c752010ac2b30e4afed051ea9643bee8ce9d8528
Alexpe77 <126241961+Alexpe77@users.noreply.github.com>
2023-10-02 16:55:26 +0200

    Bootstrap link in success is not working

--- view/success.php
+++ view/success.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./startbootstrap/css/styles.css" rel="stylesheet" />
+    <link href="./startbootstrap/css/styles.css" type="text/css" rel="stylesheet" />
     <title>Success</title>
 </head>
 
